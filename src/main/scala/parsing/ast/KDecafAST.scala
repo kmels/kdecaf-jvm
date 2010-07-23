b413f4c9c269debef26b80deaba5d5d787750fd0
Carlos Lopez <c.lopez@kmels.net>
2010-07-22 22:13:58 -0600

    Fixed ensime file, added ASTs class nodes for MethodDeclaration

--- src/main/scala/parsing/ast/KDecafAST.scala
+++ src/main/scala/parsing/ast/KDecafAST.scala
@@ -1,9 +1,17 @@
 package parsing.ast
 
+/**
+ * ASTs class nodes
+ *
+ * @author Carlos Lopez
+ * @version 1.0
+ * @since 1.0
+ */
+
 sealed class KDecafAST
 
 case class Program(val name:String, val declarations:List[Declaration]) extends KDecafAST{
-  override def toString = "Program("+name+declarations.mkString("\n")+")"
+  override def toString = "Program("+name+","+declarations.mkString("\n")+")"
 }
 
 abstract class Declaration
@@ -12,18 +20,20 @@ case class VarDeclaration(val varType:VarType[_], val id:String) extends Declara
 
 case class StructDeclaration(val name:String, val value:Struct) extends Declaration
 
-//case class MethodDeclaration(val methodType:MethodType,val name:String,val parameters:List[Parameter],val codeBlock:Block) extends Declaration
+case class MethodDeclaration(val methodType:VarType[_],val name:String,val parameters:List[Parameter],val codeBlock:Block) extends Declaration
 
 abstract class VarType[+T]{
   val value:T
 }
 
+abstract class PrimitiveType[+T] extends VarType[T]
+
 //basic types
-case class int(val value:Int) extends VarType[Int]
+case class int(val value:Int) extends PrimitiveType[Int]
 
-case class char(val value:Char) extends VarType[Char]
+case class char(val value:Char) extends PrimitiveType[Char]
 
-case class boolean(val value:Boolean) extends VarType[Boolean]
+case class boolean(val value:Boolean) extends PrimitiveType[Boolean]
 
 case class struct(val value:String) extends VarType[String] //the name of the struct
 
@@ -35,3 +45,10 @@ case class KArray[U <: VarType[_]](val value:Array[U]) extends TypeConstructor[A
 
 case class Struct(val value:List[VarDeclaration]) extends TypeConstructor[List[VarDeclaration]]
 
+case class Parameter(val varType:PrimitiveType[_], val name:String)
+
+case class ArrayParameter(val varType:KArray[_ <: VarType[_]], val name:String)
+
+case class Block(val varDeclarations:List[VarDeclaration], val statements:List[Statement])
+
+abstract class Statement
