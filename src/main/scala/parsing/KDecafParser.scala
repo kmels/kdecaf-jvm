0b2b5a3236deecec2613be3dd0210e81f111c3f2
Carlos Lopez <c.lopez@kmels.net>
2010-07-22 21:56:43 -0600

    Refactored and re-designed KDecaf type hierarchy

--- src/main/scala/parsing/KDecafParser.scala
+++ src/main/scala/parsing/KDecafParser.scala
@@ -4,26 +4,50 @@ import scala.util.parsing.combinator.{syntactical,lexical}
 import syntactical.StandardTokenParsers
 import ast._
 
+/**
+ * A Parser for the Decaf language
+ *
+ * @author Carlos Lopez
+ * @version 1.0
+ * @since 1.0
+ */ 
+
 object KDecafParser extends StandardTokenParsers{  
   override val lexical = new KDecafLexer
   
-  def program = "class" ~> ident ~ declarations ^^ { case programName~declarations => Program(programName,declarations)}
+  def program:Parser[Program] = "class" ~> ident ~ declarations ^^ { case programName~declarations => Program(programName,declarations)}
 
   def declarations:Parser[List[Declaration]] = "{" ~> rep(declaration) <~ "}"
 
-  def declaration:Parser[Declaration] = varDeclaration// | //methodDeclaration
+  def declaration:Parser[Declaration] = varDeclaration | structDeclaration // | //methodDeclaration
 
   def varDeclarations:Parser[List[VarDeclaration]] = "{" ~> rep1(varDeclaration) <~ "}"
 
-  def varDeclaration:Parser[VarDeclaration] = varArrayDeclaration | varType ~ ident <~ ";" ^^ { case varType~id => VarDeclaration(varType,id)} 
+  def varDeclaration:Parser[VarDeclaration] = varArrayDeclaration | varType ~ ident <~ ";" ^^ { case varType~id => VarDeclaration(varType,id)} | structVarDeclaration | structConstructorVarDeclaration
 
-  def varArrayDeclaration:Parser[VarDeclaration] = varType ~ ident ~ arraySizeDeclaration  ^^ { case varType~id~arraySize => VarDeclaration(ArrayOf(varType,arraySize),id)}
+  def structConstructorVarDeclaration:Parser[VarDeclaration] = structDeclaration ~ ident <~ ";" ^^ {
+    case structDeclaration~id => VarDeclaration(structDeclaration.value,id)
+  }
+
+  def structVarDeclaration:Parser[VarDeclaration] = "struct" ~> ident ~ ident <~ ";" ^^ {
+    case structName ~ id => VarDeclaration(struct(structName),id)
+  }
+  
+  def varArrayDeclaration:Parser[VarDeclaration] = varType ~ ident ~ arraySizeDeclaration ^^ {
+    case varType~id~arraySize => 
+      VarDeclaration(
+	KArray(Array.fill(arraySize)(varType)) //varType has it's value set to default value
+	,id
+      )      
+  }
 
   def arraySizeDeclaration:Parser[Int] = "[" ~> numericLit <~ "]" <~ ";" ^^ (_.toInt)
-
-  def structDeclaration:Parser[StructDeclaration] = "struct" ~> ident ~ varDeclarations ^^ { case structName~varDeclarations => StructDeclaration(structName, struct(varDeclarations))}
+  
+  def structDeclaration:Parser[StructDeclaration] = "struct" ~> ident ~ varDeclarations ^^ { 
+    case structName~varDeclarations => StructDeclaration(structName, Struct(varDeclarations))
+  }
  
-  def varType:Parser[VarType] = "int" ^^ { _ => int() } | "char" ^^ { _ => char()} | "boolean" ^^ {_ => boolean()} | "void" ^^ {_ => void()} //| structDeclaration 
+  def varType[T <: AnyVal]:Parser[VarType[AnyVal]] = "int" ^^ { _ => int(0) } | "char" ^^ { _ => char(' ')} | "boolean" ^^ {_ => boolean(false)}// | "void" ^^ {_ => void({})} 
 
   def parseTokens[T <: lexical.Scanner](tokens:T) = program(tokens)
 
--- src/main/scala/parsing/ast/KDecafAST.scala
+++ src/main/scala/parsing/ast/KDecafAST.scala
@@ -2,18 +2,36 @@ package parsing.ast
 
 sealed class KDecafAST
 
-case class Program(val name:String, val declarations:List[Declaration]) extends KDecafAST
+case class Program(val name:String, val declarations:List[Declaration]) extends KDecafAST{
+  override def toString = "Program("+name+declarations.mkString("\n")+")"
+}
 
 abstract class Declaration
 
-case class VarDeclaration(val T:VarType, val id:String) extends Declaration
-case class StructDeclaration(val name:String, val struct:struct) extends Declaration
+case class VarDeclaration(val varType:VarType[_], val id:String) extends Declaration
 
-abstract class VarType
-//types
-case class int() extends VarType
-case class char() extends VarType
-case class boolean() extends VarType
-case class void() extends VarType
-case class ArrayOf(val T:VarType, val size:Int) extends VarType
-case class struct(val varDeclarations:List[VarDeclaration]) extends VarType
+case class StructDeclaration(val name:String, val value:Struct) extends Declaration
+
+//case class MethodDeclaration(val methodType:MethodType,val name:String,val parameters:List[Parameter],val codeBlock:Block) extends Declaration
+
+abstract class VarType[+T]{
+  val value:T
+}
+
+//basic types
+case class int(val value:Int) extends VarType[Int]
+
+case class char(val value:Char) extends VarType[Char]
+
+case class boolean(val value:Boolean) extends VarType[Boolean]
+
+case class struct(val value:String) extends VarType[String] //the name of the struct
+
+case class void(val value:Unit) extends VarType[Unit]
+
+trait TypeConstructor[+T] extends VarType[T]
+
+case class KArray[U <: VarType[_]](val value:Array[U]) extends TypeConstructor[Array[U]]
+
+case class Struct(val value:List[VarDeclaration]) extends TypeConstructor[List[VarDeclaration]]
+
